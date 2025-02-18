90ff5ec67e85df4fd74dca9e6920671b4461250d
asa9874 <pjkpjk1129@gmail.com>
2025-02-18 14:13:59 +0900

    Chore:프로젝트 초기 설정 및 기본 파일 추가

---
+++ src/main/java/com/shopping/shoppingmall_backend/ShoppingmallBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.shoppingmall_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingmallBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopping/shoppingmall_backend/ShoppingmallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.shoppingmall_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
