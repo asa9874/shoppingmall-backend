81a7f01ddb00e6c0b9326eed202503f6b9703856
asa9874 <pjkpjk1129@gmail.com>
2025-03-05 13:59:41 +0900

    Fix: Review @ID 어노테이션 수정 , ReviewRepository 불필요메소드 제거거

--- src/main/java/com/shopping/model/Review.java
+++ src/main/java/com/shopping/model/Review.java
@@ -1,10 +1,10 @@
 package com.shopping.model;
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
--- src/main/java/com/shopping/repository/ReviewRepository.java
+++ src/main/java/com/shopping/repository/ReviewRepository.java
@@ -1,12 +1,9 @@
 package com.shopping.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shopping.model.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    Optional<Review> findByMemberId(Long memberId);
 }
 
