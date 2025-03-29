bb4cc219a5b6384bd95484df0c4a662facb7c735
asa9874 <pjkpjk1129@gmail.com>
2025-03-29 17:34:30 +0900

    Feat: ChatMessage 모델 추가

---
+++ src/main/java/com/shopping/model/ChatMessage.java
@@ -0,0 +1,26 @@
+package com.shopping.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
