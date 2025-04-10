4c029f5505d53959653872444a64ee95def98a97
asa9874 <pjkpjk1129@gmail.com>
2025-04-10 20:22:54 +0900

    Feat: WebClient 빈 추가 및 설정

--- src/main/java/com/shopping/config/WebConfig.java
+++ src/main/java/com/shopping/config/WebConfig.java
@@ -1,7 +1,9 @@
 package com.shopping.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:C:/uploads/");
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.build();
     }
 }
